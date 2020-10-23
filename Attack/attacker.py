f4cf514e8f6ce4765948207e7f5545c23ccc36e8
farid-eidelbeyrouthy <73123921+farid-eidelbeyrouthy@users.noreply.github.com>
2020-10-23 14:16:54 +0300

    Update attacker.py

--- Attack/attacker.py
+++ Attack/attacker.py
@@ -2,10 +2,10 @@ class Attacker:
     '''
     Initializes Attacker according the the info we get from running discovery
     '''
-
     def __init__(self, info):
         self.ip = info[0]
         self.mac = info[1]
 
+        
     def toString(self):
         return "{P}Attacker IP: {W}" + self.ip + "{P}; MAC: {W}" + self.mac
