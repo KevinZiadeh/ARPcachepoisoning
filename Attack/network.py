5ddc59bace4694fbe407bb9ceca9db6d712691ef
farid-eidelbeyrouthy <73123921+farid-eidelbeyrouthy@users.noreply.github.com>
2020-10-23 14:17:50 +0300

    Update network.py

--- Attack/network.py
+++ Attack/network.py
@@ -1,12 +1,15 @@
 class Network:
     '''
     Initializes Network according the the info we get from running discovery
+    
     '''
 
+    
     def __init__(self, info):
         self.ip = info[0]
         self.mac = info[1]
         self.broadcastIP = info[2]
 
+        
     def toString(self):
         return " {P}Router IP: {W}" + self.ip + "{P}; MAC: {W}" + self.mac + "{P}; BroadcastIP: {W}" + self.broadcastIP
